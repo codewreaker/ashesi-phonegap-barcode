a5681173b8156ddb3ea394f15a1875d76beedd9b
Israel Agyeman-Prempeh <israelagyeman-prempeh@Israels-MacBook-Pro.local>
2015-10-16 14:00:00 +0000

    added the function onload

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-function onLoad() {
-    document.addEventListener("deviceready", onDeviceReady, false);
 
+function onLoad() {
+    document.addEventListener("deviceready", deviceReady, false);
 }
 
-// device APIs are available
-//
-function onDeviceReady() {
-    $(function() {
-        barcode();
+var barcode = function () {
+    $("#scan-btn").click(function () {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                $("#textDisplay").val("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+            },
+            function (error) {
+                $("#textDisplay").val("Scanning failed: " + error);
+            }
+        );
     });
 
-    var barcode = function () {
-        $("#scan-btn").click(function () {
-            cordova.plugins.barcodeScanner.scan(
-                function (result) {
-                    $("#textDisplay").val("We got a barcode\n" +
-                        "Result: " + result.text + "\n" +
-                        "Format: " + result.format + "\n" +
-                        "Cancelled: " + result.cancelled);
-                },
-                function (error) {
-                    $("#textDisplay").val("Scanning failed: " + error);
-                }
-            );
-        });
-    }
 }
-
+ var deviceReady = $(function () {
+    barcode();
+});
